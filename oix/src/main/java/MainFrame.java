fcd172ef00dee6c668afa653695cf675e2bc67b4
plbartnicki <plbartnicki@gmail.com>
2017-08-03 21:08:24 +0200

    code update

--- oix/src/main/java/MainFrame.java
+++ oix/src/main/java/MainFrame.java
@@ -18,7 +18,7 @@ public class MainFrame extends JFrame implements ActionListener{
     private final static int NUM_OF_BUTTONS = 9;
     //ref na aktualnego gracza
     private PLAYER player = PLAYER.O; //informacja ktory gracz ma kolejny ruch
-    private int turn = 1;  //dzieki tej zmiennej mozna sprawdzic czy jest remis
+    private int turn = 0;  //dzieki tej zmiennej mozna sprawdzic czy jest remis
 
     private void createButtons() {
         for(int i = 0; i < NUM_OF_BUTTONS; i++) {
@@ -39,6 +39,7 @@ public class MainFrame extends JFrame implements ActionListener{
         setMaximumSize(new Dimension(800, 800));
         setLocation(100, 100);
         setVisible(true);
+
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
@@ -77,15 +78,35 @@ public class MainFrame extends JFrame implements ActionListener{
     //funkcja zwraca informacje o wygranej danego gracza gdy
     //w danym rzedzie albo przekatnej istnieja takie same symbole s
     private GAME_RESULT checkWinner(PLAYER player) {
-            String s = player.toString();
-
-//        for(int i = 0; i < 3; i++) {
-//            if(buttons.get(i).getText().equals(s)) {
-//                //jesli w itym przycisku jest x
-//            }
-//        }
-
-        return null;
+        String s = player.toString();
+        boolean winner = false;
+        //3i, 3i+1, 3i+2
+        for(int i = 0; i < 3; i++) {
+            //sprawdzenie czy w i-tym wierszu istnieja 3 takie same symbole
+            if(buttons.get(3*i).getText().equals(s) && buttons.get(3*i+1).getText().equals(s) && buttons.get(3*i+2).getText().equals(s)){
+                winner = true;
+                break;
+            }
+            //i, i+3, i+6
+            if(buttons.get(i).getText().equals(s) && buttons.get(i+3).getText().equals(s) && buttons.get(i+6).getText().equals(s)){
+                winner = true;
+                break;
+            }
+        }        //sprawdzenie czy dany gracz wygral "na przekatnej"
+        if(buttons.get(0).getText().equals(s) && buttons.get(4).getText().equals(s) && buttons.get(8).getText().equals(s) ||
+                buttons.get(2).getText().equals(s) && buttons.get(4).getText().equals(s) && buttons.get(6).getText().equals(s) ){
+            winner = true;
+        }
+        if(winner) {
+            return GAME_RESULT.WIN;
+        } else {
+            //jest remis
+            if(turn == 9){
+                return GAME_RESULT.DRAW;
+            } else {
+                return GAME_RESULT.NOBODY_WON;
+            }
+        }
     }
 
 }
